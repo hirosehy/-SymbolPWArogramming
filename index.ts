67142e66a814b89463e451482142b81d829d47fa
hirosehy <hirose.hy@gmail.com>
2019-08-31 12:49:22 +0900

    update: numberToSymbolメソッド追加

--- index.ts
+++ index.ts
@@ -11,6 +11,16 @@ class Symbolization {
     if (digits < number.length) throw new Error('The given number is greater than the specified number of digits')
     return ('0'.repeat(digits) + number).slice(-digits)
   }
+
+  private numberToSymbol(number: number): string {
+    if (number === 0) {
+      return '+[]'
+    } else if (number < 10 && number > 0) {
+      return '-~'.repeat(number) + '[]'
+    } else {
+      throw new Error('Number must be between 0 and 9')
+    }
+  }
 }
 
 const symbolization = new Symbolization()
